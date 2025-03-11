3e870f1c7b5eae5827f85d43a36c8d52f05d33a9
77menzs <77menzs@gmail.com>
2025-03-11 08:49:08 +0100

    Added content to Event class

--- EventEaseApp/Models/Event.cs
+++ EventEaseApp/Models/Event.cs
@@ -9,4 +9,9 @@ public class Event
         public DateTime Date { get; set; }
         [Required]
         public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<string> Attendees { get; set; } = new List<string>();
+
 }
