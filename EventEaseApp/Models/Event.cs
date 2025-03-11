a9c95fea40c20a90ffc8ddb8bb0b2272e9afe3cc
77menzs <77menzs@gmail.com>
2025-03-11 13:22:39 +0100

    Add Attendee List

--- EventEaseApp/Models/Event.cs
+++ EventEaseApp/Models/Event.cs
@@ -6,13 +6,13 @@ public class Event
 {
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime Date { get; set; }
         [Required]
-        public string Description { get; set; }
-        public string Location { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime CreatedAt { get; set; }
-        public List<string> Attendees { get; set; } = new List<string>();
+        public List<Attendee> Attendees { get; set; } = new List<Attendee>();
 
 }
