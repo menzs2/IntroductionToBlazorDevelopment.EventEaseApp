8e437b7da83e478338b9bb32a4186bd6ed1ba1cb
77menzs <77menzs@gmail.com>
2025-03-11 08:39:25 +0100

    Show event card in event list

---
+++ EventEaseApp/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApp;
+
+public class Event
+{
+        [Required]
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        [Required]
+        public string Description { get; set; }
+}
